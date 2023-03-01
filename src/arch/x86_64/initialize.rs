31b774bcb4e69b762992e3cf409a183bff6b5a0e
bmac2700 <bmac2700@proton.me>
2023-03-01 19:40:08 +0200

    x86_64: Made qemu use uefi instead of legacy bios

--- src/arch/x86_64/initialize.rs
+++ src/arch/x86_64/initialize.rs
@@ -2,15 +2,5 @@ pub fn initialize() -> Result<(), ()> {
     super::exceptions::initialize_exceptions()?;
     super::paging::initialize_paging()?;
 
-    static HELLO: &[u8] = b"Hello World!";
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
     Ok(())
 }
